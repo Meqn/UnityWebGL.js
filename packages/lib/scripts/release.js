8616df4a8d2ccbd4df1922022d68554e8f1666b9
Mervin <mengqing723@gmail.com>
2022-09-04 20:27:21 +0800

    build: add release

--- packages/lib/scripts/release.js
+++ packages/lib/scripts/release.js
@@ -6,7 +6,8 @@ const resolve = (dir) => path.resolve(__dirname, '..', dir)
 async function createVueModule() {
   try {
     await fse.mkdirp(resolve('vue'))
-    fse.copy(resolve('dist/VueUnity.esm.js'), resolve('vue/index.js'))
+    await fse.copy(resolve('dist/VueUnity.esm.js'), resolve('vue/index.js'))
+    await fse.copy(resolve('README.md'), resolve('../../README.md'), { overwrite: true })
   } catch (error) {
     throw new Error(error.message)
   }
