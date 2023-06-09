0649272668a70c14f0215730772cee30b37000a0
Mervin <mengqing723@gmail.com>
2023-06-09 11:40:53 +0800

    chore: add Jest configration & deps

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  // roots: ['<rootDir>'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '@lib/(.*)': '<rootDir>/packages/lib/$1',
+    '@vue/(.*)': '<rootDir>/packages/vue/$1'
+  },
+  setupFiles: ['jest-canvas-mock'],
+  collectCoverage: true // 是否显示覆盖率报告
+}
